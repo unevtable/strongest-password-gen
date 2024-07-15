608eb2e5f5f3230d102858fb2a952153930d534d
Sanchit Mahi <mahisanchit7@gmail.com>
2024-07-15 08:53:32 +0530

    updated

---
+++ index.js
@@ -0,0 +1,119 @@
+const characters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '~',
+  '`',
+  '!',
+  '@',
+  '#',
+  '$',
+  '%',
+  '^',
+  '&',
+  '*',
+  '(',
+  ')',
+  '_',
+  '-',
+  '+',
+  '=',
+  '{',
+  '[',
+  '}',
+  ']',
+  ',',
+  '|',
+  ':',
+  ';',
+  '<',
+  '>',
+  '.',
+  '?',
+  '/',
+];
+
+let pass1El = document.getElementById('pass1');
+let pass2El = document.getElementById('pass2');
+
+let pass1 = '';
+let pass2 = '';
+
+function generateRandom() {
+  let randomNumber = Math.floor(Math.random() * 91);
+  return randomNumber;
+}
+
+function generatePasswords() {
+  let generatedPassword = '';
+  for (let i = 0; i < 15; i++) {
+    generatedPassword += characters[generateRandom()];
+  }
+  return generatedPassword;
+}
+
+function showPassword() {
+  pass1 = generatePasswords();
+  pass2 = generatePasswords();
+  pass1El.textContent = pass1;
+  pass2El.textContent = pass2;
+}
